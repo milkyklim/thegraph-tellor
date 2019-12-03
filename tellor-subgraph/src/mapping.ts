1eb8ccd54d7119c8bc60b85ad040fa0b31cb060e
milkyklim <klim.kolyvanov@gmail.com>
2019-12-03 17:25:01 +0100

    fix id and make them unique

--- tellor-subgraph/src/mapping.ts
+++ tellor-subgraph/src/mapping.ts
@@ -1,11 +1,10 @@
 import { Transfer } from '../generated/Tellor/Tellor'
 import { Transfer as TransferEvent } from '../generated/schema'
+import { createId } from './utils'
 
 export function handleTransfer(event: Transfer): void {
-  // TODO: check this one is unique
-  let transferEvent = new TransferEvent(
-    event.params._event.transactionLogIndex.toHex(),
-  )
+  let id = createId(event.transaction.hash, event.logIndex)
+  let transferEvent = new TransferEvent(id)
   transferEvent.from = event.params._from
   transferEvent.to = event.params._to
   transferEvent.amount = event.params._value
