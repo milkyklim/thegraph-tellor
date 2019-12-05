b03c565c6878210c54ec52c04a3677e1cedcc674
milkyklim <klim.kolyvanov@gmail.com>
2019-12-05 03:27:34 +0100

    add support for miner's status

--- tellor-subgraph/src/mapping.ts
+++ tellor-subgraph/src/mapping.ts
@@ -1,12 +1,73 @@
-import { Transfer } from '../generated/Tellor/Tellor'
-import { Transfer as TransferEvent } from '../generated/schema'
-import { createId } from './utils'
+import { NewStake, StakeWithdrawn, StakeWithdrawRequested, Transfer as TransferEvent } from '../generated/Tellor/Tellor'
+import { Block, Miner, Transaction, Transfer } from '../generated/schema'
+import { createBlock, createTransaction, createId } from './utils'
+import { EthereumBlock, EthereumTransaction, EthereumEvent } from '@graphprotocol/graph-ts'
 
-export function handleTransfer(event: Transfer): void {
+export function handleNewStake(event: NewStake): void {
+  let block = createBlock(event.block)
+  let transaction = createTransaction(event.transaction, event.block)
+
+  let id = event.transaction.from.toHex()
+  let miner = new Miner(id)
+  miner.transaction = transaction.id
+  miner.status = 'STAKED'
+
+  if (Block.load(block.id) == null) {
+    block.save()
+  }
+  transaction.save()
+  miner.save()
+}
+
+export function handleStakeWithdrawn(event: StakeWithdrawn): void {
+  let block = createBlock(event.block)
+  let transaction = createTransaction(event.transaction, event.block)
+
+  let id = event.transaction.from.toHex()
+  let miner = Miner.load(id)
+  if (miner != null) {
+    miner.transaction = transaction.id
+    miner.status = 'NOT_STAKED'
+    if (Block.load(block.id) == null) {
+      block.save()
+    }
+    transaction.save()
+    miner.save()
+  }
+}
+
+export function handleStakeWithdrawRequested(event: StakeWithdrawRequested): void {
+  let block = createBlock(event.block)
+  let transaction = createTransaction(event.transaction, event.block)
+
+  let id = event.transaction.from.toHex()
+  let miner = Miner.load(id)
+  if (miner != null) {
+    miner.transaction = transaction.id
+    miner.status = 'LOCKED_FOR_WITHDRAW'
+    if (Block.load(block.id) == null) {
+      block.save()
+    }
+    transaction.save()
+    miner.save()
+  }
+}
+
+// this one is to keep track of transfers
+export function handleTransfer(event: TransferEvent): void {
+  let block = createBlock(event.block)
+  let transaction = createTransaction(event.transaction, event.block)
+
   let id = createId(event.transaction.hash, event.logIndex)
-  let transferEvent = new TransferEvent(id)
-  transferEvent.from = event.params._from
-  transferEvent.to = event.params._to
-  transferEvent.amount = event.params._value
-  transferEvent.save()
+  let transfer = new Transfer(id)
+  transfer.from = event.params._from
+  transfer.to = event.params._to
+  transfer.amount = event.params._value
+  transfer.transaction = transaction.id.toString()
+
+  if (Block.load(block.id) == null) {
+    block.save()
+  }
+  transaction.save()
+  transfer.save()
 }
