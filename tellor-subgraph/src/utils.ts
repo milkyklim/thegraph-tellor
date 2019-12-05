977e1b321e9de25e23438fcbba8e30493221b8ab
milkyklim <klim.kolyvanov@gmail.com>
2019-12-05 03:27:34 +0100

    add support for miner's status

--- tellor-subgraph/src/utils.ts
+++ tellor-subgraph/src/utils.ts
@@ -1,4 +1,5 @@
-import { BigInt, Bytes } from '@graphprotocol/graph-ts'
+import { BigInt, Bytes, EthereumBlock, EthereumTransaction } from '@graphprotocol/graph-ts'
+import { Block, Miner, Transaction as TransactionEvent, Transfer, Transaction } from '../generated/schema'
 
 // make a number the specified number of digits
 export function rightPad(str: String, size: i32): String {
@@ -11,4 +12,17 @@ export function rightPad(str: String, size: i32): String {
 // make a derived ID from tx and logIndex
 export function createId(tx: Bytes, logIndex: BigInt): String {
   return rightPad(logIndex.toHex(), 40) + '-' + tx.toHex()
+}
+
+export function createTransaction(ethereumTransaction: EthereumTransaction, ethereumBlock: EthereumBlock): Transaction {
+  let transaction = new Transaction(ethereumTransaction.hash.toHex())
+  transaction.block = ethereumBlock.hash.toHex()
+  return transaction
+}
+
+export function createBlock(ethereumBlock: EthereumBlock): Block {
+  let block = new Block(ethereumBlock.hash.toHex())
+  block.timestamp = ethereumBlock.timestamp
+  block.number = ethereumBlock.number
+  return block
 }
