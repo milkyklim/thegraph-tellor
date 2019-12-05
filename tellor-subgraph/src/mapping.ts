aefe37d13a9d3ccdec612b5adab0aac6bc4f8af1
milkyklim <klim.kolyvanov@gmail.com>
2019-12-05 21:23:11 +0100

    add full logic to parse all miner's states

--- tellor-subgraph/src/mapping.ts
+++ tellor-subgraph/src/mapping.ts
@@ -1,13 +1,19 @@
-import { NewStake, StakeWithdrawn, StakeWithdrawRequested, Transfer as TransferEvent } from '../generated/Tellor/Tellor'
+import {
+  DisputeVoteTallied,
+  NewDispute,
+  NewStake,
+  StakeWithdrawn,
+  StakeWithdrawRequested,
+  Transfer as TransferEvent,
+} from '../generated/Tellor/Tellor'
 import { Block, Miner, Transaction, Transfer } from '../generated/schema'
 import { createBlock, createTransaction, createId } from './utils'
-import { EthereumBlock, EthereumTransaction, EthereumEvent } from '@graphprotocol/graph-ts'
 
 export function handleNewStake(event: NewStake): void {
   let block = createBlock(event.block)
   let transaction = createTransaction(event.transaction, event.block)
 
-  let id = event.transaction.from.toHex()
+  let id = event.params._sender.toHex()
   let miner = new Miner(id)
   miner.transaction = transaction.id
   miner.status = 'STAKED'
@@ -23,7 +29,7 @@ export function handleStakeWithdrawn(event: StakeWithdrawn): void {
   let block = createBlock(event.block)
   let transaction = createTransaction(event.transaction, event.block)
 
-  let id = event.transaction.from.toHex()
+  let id = event.params._sender.toHex()
   let miner = Miner.load(id)
   if (miner != null) {
     miner.transaction = transaction.id
@@ -40,11 +46,51 @@ export function handleStakeWithdrawRequested(event: StakeWithdrawRequested): voi
   let block = createBlock(event.block)
   let transaction = createTransaction(event.transaction, event.block)
 
-  let id = event.transaction.from.toHex()
+  let id = event.params._sender.toHex()
   let miner = Miner.load(id)
   if (miner != null) {
     miner.transaction = transaction.id
     miner.status = 'LOCKED_FOR_WITHDRAW'
+    if (Block.load(block.id) == null) {
+      block.save()
+    }
+    transaction.save()
+    miner.save()
+  }
+}
+
+export function handleNewDispute(event: NewDispute): void {
+  let block = createBlock(event.block)
+  let transaction = createTransaction(event.transaction, event.block)
+
+  let id = event.params._miner.toHex()
+  let miner = Miner.load(id)
+  // TODO: should never be the case
+  if (miner != null) {
+    miner.transaction = transaction.id
+    miner.status = 'ON_DISPUTE'
+    if (Block.load(block.id) == null) {
+      block.save()
+    }
+    transaction.save()
+    miner.save()
+  }
+}
+
+export function handleDisputeVoteTallied(event: DisputeVoteTallied): void {
+  let block = createBlock(event.block)
+  let transaction = createTransaction(event.transaction, event.block)
+
+  let id = event.params._reportedMiner.toHex()
+  let miner = Miner.load(id)
+  // TODO: should never be the case
+  if (miner != null) {
+    miner.transaction = transaction.id
+    if (event.params._active) {
+      miner.status = 'NOT_STAKED'
+    } else {
+      miner.status = 'STAKED'
+    }
     if (Block.load(block.id) == null) {
       block.save()
     }
