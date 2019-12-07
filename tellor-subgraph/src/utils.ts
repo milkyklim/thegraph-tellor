f2f3cfe30e4f3f1c5013d89d1430503ccbbeab7d
milkyklim <klim.kolyvanov@gmail.com>
2019-12-07 18:00:26 +0100

    add storage variable reading

--- tellor-subgraph/src/utils.ts
+++ tellor-subgraph/src/utils.ts
@@ -1,5 +1,6 @@
 import { BigInt, Bytes, EthereumBlock, EthereumTransaction } from '@graphprotocol/graph-ts'
 import { Block, Miner, Transaction as TransactionEvent, Transfer, Transaction } from '../generated/schema'
+import { ByteArray } from '@graphprotocol/graph-ts'
 
 // make a number the specified number of digits
 export function rightPad(str: String, size: i32): String {
@@ -28,4 +29,15 @@ export function createBlock(ethereumBlock: EthereumBlock): Block {
   block.timestamp = ethereumBlock.timestamp
   block.number = ethereumBlock.number
   return block
+}
+
+export function stringToUTF8(string: String): ByteArray {
+  // AssemblyScript counts a null terminator, we don't want that.
+  let len = string.lengthUTF8 - 1
+  let utf8 = string.toUTF8()
+  let bytes = new ByteArray(len)
+  for (let i: i32 = 0; i < len; i++) {
+    bytes[i] = load<u8>(utf8 + i)
+  }
+  return bytes
 }
